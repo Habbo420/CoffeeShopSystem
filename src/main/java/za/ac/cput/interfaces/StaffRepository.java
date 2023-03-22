7c9aba36919b7cacf561090416da8af3bc3d1943
EthanBotes <103573130+EthanBotes@users.noreply.github.com>
2023-03-22 15:51:01 +0200

    Add files via upload
    Ethan first commit assignment 1. Staff and Stafftype

---
+++ src/main/java/za/ac/cput/interfaces/StaffRepository.java
@@ -0,0 +1,34 @@
+package za.ac.cput.interfaces;
+
+import za.ac.cput.domain.Staff;
+
+import java.util.Set;
+
+public class StaffRepository implements InterfaceStaffRepository{
+
+
+    @Override
+    public Staff create(Staff staff) {
+        return null;
+    }
+
+    @Override
+    public Staff read(String s) {
+        return null;
+    }
+
+    @Override
+    public Staff update(Staff staff) {
+        return null;
+    }
+
+    @Override
+    public Staff delete(Staff staff) {
+        return null;
+    }
+
+    @Override
+    public Set<Staff> getAll() {
+        return null;
+    }
+}
